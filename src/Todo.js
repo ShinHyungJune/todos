02b26cc39526446edea359dd3a8a3f60e8774305
ShinHyungJune <ssa4141@naver.com>
2020-02-05 15:35:11 +0900

    crud

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,69 @@
-// @flow
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-const Todo = ({todo}) => (
-  <div className="todo">
-      <p className="title">할일명: {todo.title}</p>
-      <p className="body">할일 상세설명: {todo.body}</p>
-  </div> 
-);
+const Todo = ({todo, todos, setTodos}) => {
+	let [updateMode, setUpdateMode] = useState(false);
+	let [form, setForm] = useState({
+		title: todo.title,
+		body: todo.body
+	});
+	
+	const changeForm = (e) => {
+		setForm({
+			...form,
+			[e.target.name] : e.target.value
+		});
+	};
+	
+	const update = (e) => {
+		e.preventDefault();
+		
+		axios.patch("/todos/" + todo.id, form)
+			.then(response => {
+				todos = todos.map(todoData => {
+					console.log(response.data);
+					if(todoData.id === response.data.id)
+						return response.data;
+					
+					return todoData;
+				});
+				
+				setTodos(todos);
+			});
+		
+		setUpdateMode(false);
+	};
+	
+	const remove = () => {
+		axios.delete("/todos/" + todo.id)
+			.then(response => {
+				todos = todos.filter(todoData => {
+					return todoData.id !== todo.id
+				});
+				
+				setTodos(todos);
+			});
+	};
+	
+	return (
+		<div className="todo">
+			{updateMode ? (
+				<form onSubmit={update}>
+					<input type="text" name="title" defaultValue={form.title} onChange={changeForm}/>
+					<br/>
+					<input type="text" name="body" defaultValue={form.body} onChange={changeForm}/>
+					<button>완료</button>
+				</form>
+			) : (
+				<div>
+					<p className="title">할일명: {todo.title}</p>
+					<p className="body">할일 상세설명: {todo.body}</p>
+					<button onClick={() => {setUpdateMode(true)}}>수정</button>
+				</div>
+			)}
+			<button onClick={remove}>삭제</button>
+		</div>
+	);
+}
 
 export default Todo;
--- src/Todos.js
+++ src/Todos.js
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Todo from './Todo';
+import axios from 'axios';
 
 const Todos = ({}) => {
-    let todos = [ // 임시로 가상 할일목록 설정
-        {
-            id: 1,
-            title: "빨래",
-            body: "수건을 복도 펜트리, 사복은 드레스룸"
-        },
-        {
-            id: 2,
-            title: "식기세척",
-            body: "식기세척기에 넣고 고온건조"
-        },
-        {
-            id: 3,
-            title: "먼지청소",
-            body: "거실과 안방 공기청정기 가동, 창문 다 열고 식탁 의자 올려놓은 다음에 로봇청소기 돌리기"
-        },
-    ];
+	let [todos, setTodos] = useState([]);
+	let [form, setForm] = useState({
+		title: "",
+		body: ""
+	});
+	
+	useEffect(() => {
+		// 페이지가 열리면 할일 목록 받아오기
+		axios.get("/todos")
+			.then(response => {
+				setTodos(response.data);
+			});
+	}, []);
+	
+	// 사용자 입력에 따른 form 데이터 변경
+	const changeForm = (e) => {
+		setForm({
+			...form,
+			[e.target.name]: e.target.value
+		});
+	};
+	
+	// 할일 생성
+	const submit = (e) => {
+		// 페이지 새로고침 막기
+		e.preventDefault();
+		
+		// 할일 생성 api 요청
+		axios.post("/todos", form)
+			.then(response => {
+				// 할일 목록에 저장한 할일 추가
+				setTodos([...todos, response.data]);
+			})
+	};
+	
+	return (
+		<div className="todos">
+			<form onSubmit={submit}>
+				<input type="text" name="title" placeholder="할일 제목" onChange={changeForm}/>
+				<input type="text" name="body" placeholder="할일 내용" onChange={changeForm}/>
+				<button>할일 생성</button>
+			</form>
+			
+			{todos.map(todo => <Todo key={todo.id} todo={todo} todos={todos} setTodos={setTodos}/>)}
+		</div>
+	);
+};
 
-    return (
-       <div className="todos">
-           {todos.map(todo => <Todo key={todo.id} todo={todo} />)}
-       </div>
-    );
-}
 export default Todos;
